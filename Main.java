4cb143ad180288dc695662a6a67c4c993da7d665
MfDevNet <45531597+MfDevNet@users.noreply.github.com>
2018-12-03 10:50:02 +0330

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+package com.MfDevNet.www;
+
+import org.junit.jupiter.api.Test;
+
+public class Main {
+    @Test
+    public static void main(String[] args) {
+        System.out.println("Hi I Start Program Java Language");
+        System.out.println("I sure Success");
+    }
+}
